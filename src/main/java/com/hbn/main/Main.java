832b8c1ca0eb9ad43a53dbcc3d600d82af3a60f8
aarav <aaravaryan24@gmail.com>
2026-07-06 14:14:56 +0530

    update

--- src/main/java/com/hbn/main/Main.java
+++ src/main/java/com/hbn/main/Main.java
@@ -15,6 +15,7 @@ public class Main {
 	public static void main(String[] args) {
 		
 		Employee e = new Employee( "Khushi", "Female", 49000);
+		Employee e1 = new Employee( "Nitish", "Male", 50000);
 	
 		// hibernate.cfg.xml is the default file name(no need to mention it), otherwise we need to mention configuration file name
 //		Configuration cfg = new Configuration().configure("hibernate.cfg.xml");   
@@ -27,6 +28,7 @@ public class Main {
 		Transaction tx = session.beginTransaction();
 	
 		session.persist(e);
+		session.persist(e1);
 		
 		tx.commit();
 	
