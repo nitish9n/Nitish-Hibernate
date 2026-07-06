4d05ea52572d8a99709854f5901b4e9637fd4c9a
Nitish Kumar <151989125+nitish9n@users.noreply.github.com>
2026-07-06 14:22:48 +0530

    Update comment for hibernate.dialect property
    Updated hibernate.dialect property comment to indicate it is now useless.

--- src/main/java/com/hbn/configuration/HibernateConfig.java
+++ src/main/java/com/hbn/configuration/HibernateConfig.java
@@ -19,7 +19,7 @@ public class HibernateConfig {
 		 // java based configuration
 		 Properties properties = new Properties();      //  Property is a legacy class of collection
 		 
-		 properties.put("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+		 properties.put("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");         // it can be ignored, now useless
 		 properties.put(Environment.JAKARTA_JDBC_URL, "jdbc:mysql://localhost:3306/mydb");
 		 properties.put(Environment.JAKARTA_JDBC_USER, "root");
 		 properties.put(Environment.JAKARTA_JDBC_PASSWORD, "root");
